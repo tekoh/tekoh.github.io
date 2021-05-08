17d72c61758e8d814309a91f583727d95624b30f
max <max.0x71@gmail.com>
2021-05-08 21:06:18 +0100

    added js for countdown

--- js/script.js
+++ js/script.js
@@ -1,6 +1,41 @@
 let copyNotificationShowing = false
+
 $(window).on("load", () => {
     $("body").removeClass("preload")
+
+    const countdown =  () => {
+        const now = new Date()
+
+        const year = now.getFullYear()
+        const month = now.getMonth() + 1
+        const day = now.getDate()
+
+        let targetDate, target, birthday, birthdayThing
+
+        if (month > 5) {
+            targetDate = new Date(Date.parse(`12/25/${year}`))
+            target = "christmas"
+        } else {
+            targetDate = new Date(Date.parse(`5/25/${year}`))
+            target = "birthday"
+            birthday = year - 2003
+            birthdayThing = birthday.toString().endsWith("1")
+                ? "st"
+                : birthday.toString().endsWith("2")
+                ? "nd"
+                : birthday.toString().endsWith("3")
+                ? "rd"
+                : "th"
+        }
+
+        $("#countdown-text")[0].innerText = `${MStoTime(
+            targetDate.getTime() - now.getTime()
+        )} until ${target == "christmas" ? "christmas" : `${birthday}${birthdayThing} birthday`}`
+    }
+
+    countdown()
+    
+    setInterval(countdown, 1000);
 })
 
 const stages = ["max#0777", "cax#077!", "cox#07!!", "cop#0d!!", "copied!!"]
@@ -45,4 +80,32 @@ function copyDiscord() {
     //     copyNotificationShowing = false
     //     $("#discord")[0].innerText = "max#0777"
     // }, 4000)
+}
+
+function MStoTime(ms) {
+    const days = Math.floor(ms / (24 * 60 * 60 * 1000))
+    const daysms = ms % (24 * 60 * 60 * 1000)
+    const hours = Math.floor(daysms / (60 * 60 * 1000))
+    const hoursms = ms % (60 * 60 * 1000)
+    const minutes = Math.floor(hoursms / (60 * 1000))
+    const minutesms = ms % (60 * 1000)
+    const sec = Math.floor(minutesms / 1000)
+
+    let output = ""
+
+    if (days > 0) {
+        output = output + days + " days "
+    }
+
+    if (hours > 0) {
+        output = output + hours + " hours "
+    }
+
+    if (minutes > 0) {
+        output = output + minutes + " minutes "
+    }
+
+    output = output + sec + " seconds"
+
+    return output
 }
